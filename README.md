33d624658b7d139300af8ec8b2c8b487631f47ac
Revathy Venugopal <104772255+Revathyvenugopal162@users.noreply.github.com>
2023-03-01 12:28:41 +0100

    FEAT: Add the variables (#1)
    * FEAT: Add the variables
    
    * FEAT : pgm loop
    
    * FEAT : functions in js
    
    * Javascript object
    
    * FEAT :  Add DOM
    
    * Update README.md

--- README.md
+++ README.md
@@ -7,7 +7,90 @@ For buisness applications and wep app.
 
 # Fixing error
 - in console
+- freely use white space
 - commenting 
 1. // ___ single comment
 2. /* */ 
 - 
+# variable and constants
+
+declaring variables
+- let
+ * int, float, boolean, string
+ * camalcase - __accountNumber__
+ * private - _accountNumber
+ * automatically generated - $accountNumber
+ * `` __proto__`` - non standard features
+- const
+ * fixed
+- var
+ * variable
+naming variables
+common error variables
+
+# Types and operators
+# pgm flow
+* if ... else - conditional
+* comparinf === ==
+* Truthy Falsy
+* The ternary Operator
+* Blockscope
+* for, while...
+
+Ternarty operators
+==================
+(condition)? true statement: false statement;
+
+Functions
+==========
+
+``` 
+function showMessage(){
+
+}
+```
+```
+showMessage();
+```
+```
+let showMessage = function(message, firstName){
+    console.log(message, firstName);
+}
+
+showMessage(message, firstName);
+```
+
+Objects and the DOM
+===================
+properties
+----------
+```
+let person = {
+    name : "Revathy",
+    hour : 40,
+    Time : True
+};
+```
+object methods
+--------------
+
+```
+let person = {
+    name : "Revathy",
+    hour : 40,
+    Time : True,
+    showInfo: function(){
+        showmessage(this.name) // property name
+    }
+};
+
+person.showInfo();
+showMessage( typeof person.showInfo) // function
+```
+
+The objects are at *_https://developer.mozilla.org/en-US/docs/Web/JavaScript_*.
+
+DOM (Document Object Model)
+----------------------------
+
+HTML element - style
--- first-code/hello.js
+++ first-code/hello.js
@@ -1,1 +1,73 @@
-showMessage("Something");
+let welcome = "Heloo";
+let value = 14,
+    nameS = 'some',
+    some = false;
+const price = 40; // fixed
+let things = `${nameS}THhing.......`
+let saved = true;
+saved = !saved;
+showMessage(typeof (things.toLowerCase()).substring(2));//strip first 2 charcters
+var pricevv = 25;
+value = value / 5 ;// -=, +=,  /=, % , ++
+console.log(pricevv);
+console.log(saved);
+let person = { // object
+    firstName: 'Revathy',
+    lastName: 'venugopal',
+};
+console.log(typeof person);
+console.log(person);
+console.log(person.lastName);
+
+// The conditional
+if ( 1 === 1){//type check
+    console.log("ifloop");
+}
+// !==, truthy and falsy
+if ( 1 == '1'){
+    console.log("Not looking for types");
+} 
+
+if(0){ //false
+    console.log("true");
+}
+else{// else if
+    console.log("false");
+}
+
+(price > 10) ? console.log('yes') : console.log('no');
+
+for(let i =0; i<3; i++){
+    console.log(i);
+}
+
+function showMessages(message, anotherMessage){
+    console.log(message, anotherMessage);
+}
+
+let key = 20;
+function getSecretCode(value) {
+    let code = value * key;
+    return code;
+}
+showMessages("This is", getSecretCode(2));
+showName("Revathy Venugopal")
+
+
+let symbol = Symbol();//secret codes
+let personOne = {
+    name: "Revathy Venugopal",
+    hour: 40,
+    Time: true,
+    [symbol]: 'SecretInformation'
+};
+
+person.hour = 50;
+
+console.log(personOne.name);
+console.log(personOne.hour);
+
+let now = new Date();
+console.log(now.toDateString());
+console.log( Math.abs(-69));
+
